125ffe9f439a8ba36919f8b4df93a97a6e9e9de3
bigblackpigpig <lwz12@outlook.com>
2021-07-07 11:19:34 +0800

    temp

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener("click", closeModal);
 
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+
+  if (e.key === "1" || e.key === "2" || e.key === "3") {
+    openModal();
+  }
+});
